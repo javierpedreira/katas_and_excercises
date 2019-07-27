75e79280ef715d764319836d90abfefb5993f845
Javier Pedreira <javier.pedreira@intenthq.com>
2019-07-27 14:42:38 +0200

    Fix case 4

--- factoriser/src/main/scala/Factorizer.scala
+++ factoriser/src/main/scala/Factorizer.scala
@@ -1,5 +1,23 @@
 import scala.collection.mutable
 
-class Factorizer(number: Int) {
-  def factorise: Seq[Int] = Seq(number)
+object Factoriser {
+  def factorise(number: Int): Seq[Int] = {
+    def innerFact(partialFactorisedNumber: Int, maybeFactor: Int): Seq[Int] = {
+      Seq(number).foldRight(Seq[Int]())((numFromSeq: Int, accum: Seq[Int]) => {
+        if (maybeFactor > 0 && partialFactorisedNumber > 0) {
+          val divisionRest  = numFromSeq % maybeFactor
+          divisionRest match {
+            case 0 => innerFact(partialFactorisedNumber - maybeFactor, maybeFactor) ++ Seq(maybeFactor) ++ accum
+            case 1 => innerFact(partialFactorisedNumber, maybeFactor + 1) ++ accum
+          }
+        } else {
+          return accum
+        }
+      })
+
+      }
+
+    println(s"Factorising $number")
+    innerFact(number, 2)
+  }
 }
--- factoriser/src/test/scala/FactorizerSpec.scala
+++ factoriser/src/test/scala/FactorizerSpec.scala
@@ -4,10 +4,14 @@ import collection.immutable.Seq
 
 class FactorizerSpec extends FunSpec with Matchers {
   it ("factor of 2 should be [2]") {
-    new Factorizer(2).factorise should be (Seq(2))
+    Factoriser.factorise(2) should be (Seq(2))
   }
 
   it ("factor of 3 should be [3]") {
-    new Factorizer(3).factorise should be (Seq(3))
+    Factoriser.factorise(3) should be (Seq(3))
+  }
+
+  it ("factor of 4 should be [2, 2]") {
+    Factoriser.factorise(4) should be (Seq(2, 2))
   }
 }
