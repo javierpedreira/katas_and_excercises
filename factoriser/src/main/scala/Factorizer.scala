e11aa9c1b6ca3433fa4e383adf69c3d307d7bfe0
Javier Pedreira <javier.pedreira@intenthq.com>
2019-07-27 14:44:56 +0200

    Changed my mind, using a pattern match + recursivity works better here

--- factoriser/src/main/scala/Factorizer.scala
+++ factoriser/src/main/scala/Factorizer.scala
@@ -3,21 +3,18 @@ import scala.collection.mutable
 object Factoriser {
   def factorise(number: Int): Seq[Int] = {
     def innerFact(partialFactorisedNumber: Int, maybeFactor: Int): Seq[Int] = {
-      Seq(number).foldRight(Seq[Int]())((numFromSeq: Int, accum: Seq[Int]) => {
-        if (maybeFactor > 0 && partialFactorisedNumber > 0) {
-          val divisionRest  = numFromSeq % maybeFactor
-          divisionRest match {
-            case 0 => innerFact(partialFactorisedNumber - maybeFactor, maybeFactor) ++ Seq(maybeFactor) ++ accum
-            case 1 => innerFact(partialFactorisedNumber, maybeFactor + 1) ++ accum
-          }
-        } else {
-          return accum
-        }
-      })
 
+      val accum = Seq[Int]()
+
+      if(partialFactorisedNumber == maybeFactor) return accum ++ Seq(maybeFactor)
+      val res = partialFactorisedNumber % maybeFactor match {
+        case 0 => Seq(maybeFactor) ++ innerFact(partialFactorisedNumber / maybeFactor, maybeFactor)
+        case _ => innerFact(partialFactorisedNumber, maybeFactor + 1)
       }
 
-    println(s"Factorising $number")
+      accum ++ res
+    }
+
     innerFact(number, 2)
   }
 }
--- factoriser/src/test/scala/FactorizerSpec.scala
+++ factoriser/src/test/scala/FactorizerSpec.scala
@@ -14,4 +14,20 @@ class FactorizerSpec extends FunSpec with Matchers {
   it ("factor of 4 should be [2, 2]") {
     Factoriser.factorise(4) should be (Seq(2, 2))
   }
+
+  it ("factor of 6 should be [2, 3]") {
+    Factoriser.factorise(6) should be (Seq(2, 3))
+  }
+
+  it ("factor of 9 should be [3, 3]") {
+    Factoriser.factorise(9) should be (Seq(3, 3))
+  }
+
+  it ("factor of 12 should be [2, 2, 3]") {
+    Factoriser.factorise(12) should be (Seq(2, 2, 3))
+  }
+
+  it ("factor of 15 should be [3, 5]") {
+    Factoriser.factorise(15) should be (Seq(3, 5))
+  }
 }
