163a4553cef8d8df22f9bb938f956b0a2964e213
Javier Pedreira <javier.pedreira@intenthq.com>
2019-07-28 13:52:39 +0200

    Cleaning a bit Factoriser code

--- factoriser/src/main/scala/Factorizer.scala
+++ factoriser/src/main/scala/Factorizer.scala
@@ -1,18 +1,15 @@
-import scala.collection.mutable
-
 object Factoriser {
   def factorise(number: Int): Seq[Int] = {
     def innerFact(partialFactorisedNumber: Int, maybeFactor: Int): Seq[Int] = {
-
       val accum = Seq[Int]()
 
-      if(partialFactorisedNumber == maybeFactor) return accum ++ Seq(maybeFactor)
-      val res = partialFactorisedNumber % maybeFactor match {
+      if(partialFactorisedNumber == maybeFactor) return Seq(maybeFactor)
+
+      (partialFactorisedNumber % maybeFactor match {
         case 0 => Seq(maybeFactor) ++ innerFact(partialFactorisedNumber / maybeFactor, maybeFactor)
         case _ => innerFact(partialFactorisedNumber, maybeFactor + 1)
-      }
+      }) ++ accum
 
-      accum ++ res
     }
 
     innerFact(number, 2)
