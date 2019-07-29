462c22ddbf0c662831cc43c5da019a791c2232ca
Javier Pedreira <javier.pedreira@intenthq.com>
2019-07-29 09:34:02 +0200

    Add exercise 2

--- scalaRedBook/src/main/scala/Chapter2.scala
+++ scalaRedBook/src/main/scala/Chapter2.scala
@@ -1,7 +1,7 @@
 import scala.annotation.tailrec
 
 object Chapter2 {
-  // exercise 2.1
+  // exercise 2.1 tail recs
   def fib(n: Int): Int = {
     @tailrec
     /*
@@ -15,4 +15,23 @@ object Chapter2 {
 
     fibTailRec(n, prev = 1, current = 0)
   }
+
+  //exercise 2.2 HOF
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    /* isSorted is a generic and can be used with any type of Array of a given object
+       in order to work it'll need an external ordered method belonging to the A type
+       that helps to decide if the array is sorted
+    * */
+    @tailrec
+    def sortedTailRec(current: Int): Boolean = {
+      if(current == as.length -1) true
+      else {
+        if(ordered(as(current), as(current + 1))) sortedTailRec(current + 1)
+        else false
+      }
+    }
+
+    sortedTailRec(0)
+  }
+
 }
--- scalaRedBook/src/test/scala/Chapter2Spec.scala
+++ scalaRedBook/src/test/scala/Chapter2Spec.scala
@@ -1,29 +1,73 @@
 import org.scalatest._
+import Chapter2._
 
 class Chapter2Spec extends FunSpec with Matchers {
   describe("Fibonacci sequence") {
     it("fibonacci 0 should be 0") {
-      Chapter2.fib(0) should be(0)
+      fib(0) should be(0)
     }
 
     it("fibonacci 1 should be 1") {
-      Chapter2.fib(1) should be(1)
+      fib(1) should be(1)
     }
 
     it("fibonacci 2 should be 1") {
-      Chapter2.fib(2) should be(1)
+      fib(2) should be(1)
     }
 
     it("fibonacci 3 should be 2") {
-      Chapter2.fib(3) should be(2)
+      fib(3) should be(2)
     }
 
     it("fibonacci 5 should be 5") {
-      Chapter2.fib(5) should be(5)
+      fib(5) should be(5)
     }
 
     it("fibonacci 6 should be 8") {
-      Chapter2.fib(6) should be(8)
+      fib(6) should be(8)
     }
+  }
+
+  describe("Polymorphic isSorted") {
+    describe("isSorted with Ints") {
+      it("[1,2,3,4,5] is an increasing sequence") {
+        isSorted[Int](Array[Int](1, 2, 3, 4), (x: Int, y: Int) => x <= y) should be (true)
+      }
+
+      it("[1, 3, 2, 5, 6] is not an increasing sequence") {
+        isSorted[Int](Array[Int](1, 3, 2, 5, 6), (x: Int, y: Int) => x <= y) should be (false)
+      }
+
+      it("[100, 50, 25] is a decreasing sequence") {
+        isSorted[Int](Array[Int](100, 50, 25), (x: Int, y: Int) => x >= y) should be (true)
+      }
+
+      it("[100, 50, 25, 200] is not a decreasing sequence") {
+        isSorted[Int](Array[Int](100, 50, 25, 200), (x: Int, y: Int) => x >= y) should be (false)
+      }
+    }
+
+    describe("isSorted with Strings") {
+      it("[a, aa, aaa, bb, ccc] is an ordered sequence") {
+        isSorted[String](Array[String]("a", "aa", "aaa", "bb", "ccc"), (x: String, y: String) => x <= y) should be (true)
+      }
+
+      it("[a, aa, zzz , aaa, bb, ccc] is not an ordered sequence") {
+        isSorted[String](Array[String]("a", "aa", "zzz", "aaa", "bb", "ccc"), (x: String, y: String) => x <= y) should be (false)
+      }
+    }
+
+    describe("isSorted with some class") {
+      class SomeTypeClass(var elem: String)
+
+      object SomeTypeClass {
+        def apply(elem: String): SomeTypeClass = new SomeTypeClass(elem)
+        def ordered(a: SomeTypeClass, b: SomeTypeClass): Boolean = a.elem < b.elem
+      }
+
+      isSorted[SomeTypeClass](Array[SomeTypeClass](SomeTypeClass("aa"), SomeTypeClass("bb"), SomeTypeClass("ccc")), SomeTypeClass.ordered) should be (true)
+
+    }
+
   }
 }
